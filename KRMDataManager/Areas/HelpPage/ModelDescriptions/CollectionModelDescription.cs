598f85bd7339c923cd8d58926df9c7f5efe76c9a
Kwesi Gepi-Attee <findkwesi@gmail.com>
2021-04-29 20:29:14 +0100

    Add the Web API Project
    Adds a blank Web API Project and tweaks it

---
+++ KRMDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace KRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
