ba0ec6aad170510cd05e837c8483921857c93c4f
Kwesi Gepi-Attee <findkwesi@gmail.com>
2021-04-30 19:31:29 +0100

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API for documentation and testing

---
+++ KRMDataManager/App_Start/AuthorisationOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace KRMDataManager.App_Start
+{
+    public class AuthorisationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                //Worth noting: I spelt authorization with an 's' and this did not work
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+
+        }
+    }
+}
