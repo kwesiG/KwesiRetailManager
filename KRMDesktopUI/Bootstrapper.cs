b40e508c94ca008e56d8e78f92f8aac4aa53b79d
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-05-27 11:39:30 +0100

    Adding WPF Project, Swapping MainView for ShellView using Caliburn Bootstrapper. Adding View, Model, ViewModel architecture

---
+++ KRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using KRMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
