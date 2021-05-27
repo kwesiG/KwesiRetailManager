c13f9ac17cae32c809d44ed04582a974a17e5ed6
Kwesi Gepi-Attee <kwesi.gepi.attee@gmail.com>
2021-05-27 20:23:52 +0100

    Add container singleton and reflection

--- KRMDesktopUI/Bootstrapper.cs
+++ KRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,49 @@ namespace KRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+        // This handles the instantiation of objects through out the project. How? I hear you ask.. No idea
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Reflection.. Have a look at what is going on here
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
